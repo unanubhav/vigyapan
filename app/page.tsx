83a972ef655b9ff04ec9914ebd66a12823e9b347
anubhav <anubhav@datagenit.com>
2024-07-13 21:36:12 +0530

    updat data

--- app/page.tsx
+++ app/page.tsx
@@ -18,13 +18,13 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
+            Docsdfdsfsfsdfs{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            asdfasdfasdfsadfsdafdsaf sdf sdf sdf
+            asdfasdfasdfsad  fsdafdsaf sdsdff sdf sdf
           </p>
         </a>
 
